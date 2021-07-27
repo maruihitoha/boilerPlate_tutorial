7abc4af7e4e5750f575e37ef1fa0732fc6de7cb4
HunDo-KIM <duckdoodle@dongguk.edu>
2021-07-27 16:48:11 +0900

    POSTMAN 체험

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
+
+const { User } = require("./models/User");
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://DOHUN:abcd123@cluster0.tf1jh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -15,6 +23,23 @@ mongoose.connect('mongodb+srv://DOHUN:abcd123@cluster0.tf1jh.mongodb.net/myFirst
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+
+    if(err) return res.json({ success: false, err});
+    return res.status(200).json({success: true});
+    
+  });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
