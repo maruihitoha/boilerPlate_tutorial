ca4c875e14057aa9c81d215eeb7001fe7bf9e602
HunDo-KIM <duckdoodle@dongguk.edu>
2021-07-28 22:35:19 +0900

    로그인기능 BCRYPT 사용

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
-// application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-// application/json
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+
+// application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+//app.use(bodyParser.json());
+app.use(cookieParser());
 
 const { User } = require("./models/User");
 
@@ -43,7 +48,61 @@ app.post('/register', (req, res) => {
 
 });
 
+app.post('/login', (req, res) => {
+
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+
+  User.findOne({email: req.body.email}, (err, user) => { // 몽고db에서 제공.
+      if(!user) 
+      {
+        return res.json({
+          loginSuccess:false,
+          message: "제공된 이메일에 해당되는 유저가 없습니다.."
+        });
+      }
+    // 데이터베이스에서 찾았다면 비밀번호를 맞춰봄
+   
+    
+      user.comparePassword(req.body.password, (err, isMatch ) => {
+        //return res.json({loginSuccess: false, message: "일단 진입."});
+        
+        if(!isMatch)  // 비밀번호가 틀렸다는 뜻 -> 로그인 석세스 : false
+        {          
+          return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+        }
+        // 비밀번호까지 맞다면 토큰을 생성하기
+        // JSONWEBTOKEN 라이브러리 사용
+        // npm install jsonwebtoken --save
+        // https://www.npmjs.com/search?q=jsonwebtoken 참조
+        
+        user.generateToken((err, user) => {
+          if(err) return res.status(400).send(err);
+
+          // 토큰을 저장한다. 어디에 ? user의 token에 담긴걸 쿠키나 로컬스토리지에 저장 가능
+          // 쿠키 파서 라이브러리 설치.
+          // npm install cookie-parser --save
+          res.cookie("x_auth", user.token) // x_auth라는 이름의 쿠키에 토큰이 들어감
+          .status(200)  // 성공
+          .json({loginSuccess: true, userId:user._id});
+
+        });
+        
+        
+
+      });
+    });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+/* {
+    "name":"Test",
+    "email":"Test@email.com",
+    "password":"TestPW"
+} */
--- models/User.js
+++ models/User.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt');
 
 // salt가 몇글자인지
 const saltRound = 10;   // Salt를 이용해서 비밀번호 암호화.
+
+// jsonwebtoken 가져옴
+const jwt = require('jsonwebtoken');
+
+
 const userSchema = mongoose.Schema({
     name: 
     {
@@ -30,8 +35,9 @@ const userSchema = mongoose.Schema({
         type : Number,
         default : 0
     },
-    image:String,
-    token : {
+    image:{type:String},
+    token : 
+    {
         type : String
     },
     tokenExp:
@@ -50,7 +56,7 @@ userSchema.pre('save', function(next){
     if(user.isModified('password'))
     {
         // genSalt. salt rounds 필요.
-        bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.genSalt(saltRound, function(err, salt) {
             if(err) return next(err);
             bcrypt.hash(user.password, salt, function(err, hash) {
                 // Store hash in your password DB.
@@ -61,10 +67,43 @@ userSchema.pre('save', function(next){
         });
 
     }
-
-   
+    else
+    {
+        next();
+    }   
     
 });
+
+// comparePassword 함수 작성                  비밀번호와 콜백함수
+userSchema.methods.comparePassword = function(plainPassword, cb)
+{
+    // userschema의 this
+    // plainPassword TestPW  암호화된 비밀 번호 : $2b$10$H9xU7IteLk/r6.v2dTmQPOU4XeE97mOVAnuZ1h63uuukG/use0iT6 
+    
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {   
+          
+        if(err) return cb(err);
+        // 에러는 없고 비밀번호는 같다. true ==> index.js의 user.compareP 로 이동
+        cb(null, isMatch);
+    });
+};
+// index.js의 user.generateToken((err, user)... 와 연결
+userSchema.methods.generateToken = function(cb) 
+{
+    // es5 문법을 사용하고 있기에 user = this 받아옴
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    // user._id + 'secretToken = token
+    // plaintext로 보내기위해 toHexString 사용
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    });
+
+}
 
 const User = mongoose.model('User', userSchema);
 
